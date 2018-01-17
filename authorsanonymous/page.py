b12470c4364c715c385e5aa86cc9928a90995bf8
Tim Heap <tim@timheap.me>
2018-01-17 10:09:47 +0200

    Add wagtail-metadata

--- authorsanonymous/page.py
+++ authorsanonymous/page.py
@@ -1,10 +1,10 @@
 from wagtail.wagtailcore.models import Page
+from wagtailmetadata.models import MetadataPageMixin
 
 
-class Page(Page):
+class Page(MetadataPageMixin, Page):
     class Meta:
         abstract = True
-
 
     def get_template(self, request):
         return 'layouts/{}.html'.format(self._meta.model_name)
