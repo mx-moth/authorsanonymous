dabd3ed2cfb300ebc3211f4515584a9d9d106ffb
Tim Heap <tim@timheap.me>
2018-07-02 21:03:19 +1000

    Add error views

--- authorsanonymous/urls.py
+++ authorsanonymous/urls.py
@@ -2,27 +2,36 @@ import wagtail.admin.urls
 import wagtail.core.urls
 import wagtail.documents.urls
 from django.conf import settings
-from django.conf.urls import include, url
 from django.conf.urls.static import static
+from django.urls import include, path
 from django.views.generic import TemplateView
+from wagtail.contrib.sitemaps.views import sitemap
 
-from . import views
+from .views import ErrorView, contact_form, contact_submit, newsletter_submit
+
+handler404 = ErrorView.as_view(template_name='layouts/404.html', status=404)
+handler500 = ErrorView.as_view(template_name='layouts/500.html', status=500)
 
 urlpatterns = [
-    url(r'^robots.txt$', TemplateView.as_view(content_type='text/plain',
-                                              template_name='robots.txt')),
-    url(r'^admin/', include(wagtail.admin.urls)),
-    url(r'^documents/', include(wagtail.documents.urls)),
-    url(r'^contact/$', views.contact_form, name='contact'),
+    path('robots.txt', TemplateView.as_view(
+        content_type='text/plain', template_name='robots.txt')),
+    path('sitemap.xml', sitemap),
 
-    url(r'^_api/contact/$',
-        views.contact_submit,
-        name='contact_submit'),
-    url(r'^_api/newsletter_signup/$',
-        views.newsletter_submit,
-        name='newsletter_signup'),
+    path('admin/', include(wagtail.admin.urls)),
+    path('documents/', include(wagtail.documents.urls)),
+    path('contact/', contact_form, name='contact'),
 
-    url(r'', include(wagtail.core.urls)),
+    path('404/', handler404),
+    path('500/', handler500),
+
+    path('_api/contact/',
+         contact_submit,
+         name='contact_submit'),
+    path('_api/newsletter_signup/',
+         newsletter_submit,
+         name='newsletter_signup'),
+
+    path('', include(wagtail.core.urls)),
 ]
 
 
--- authorsanonymous/views.py
+++ authorsanonymous/views.py
@@ -1,11 +1,23 @@
 from django.http.response import Http404, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
+from django.views.generic.base import ContextMixin, TemplateResponseMixin, View
 from mailchimp3.mailchimpclient import MailChimpError
 from requests.exceptions import RequestException
 
 from .forms import ContactForm, NewsletterSubscribeForm
 from .models import MailchimpSettings
+
+
+class ErrorView(TemplateResponseMixin, ContextMixin, View):
+    """
+    Render a template. Pass keyword arguments from the URLconf to the context.
+    """
+    status = 200
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context, status=self.status)
 
 
 def contact_form(request):
